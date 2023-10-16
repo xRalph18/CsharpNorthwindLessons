8a4aabda4128a33880f5668ac8063864318b6b6a
xRalph18 <rafal.machnik4@gmail.com>
2023-10-16 16:29:41 +0200

    dapper

---
+++ BazaDanychNorthWind_Dapper - 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using BazaDanychNorthWind_Dapper___2;
+using Dapper;
+using System.Data.SqlClient;
+
+class Program
+{
+    public static void Main()
+    {
+        string connString = "Data Source=LAPTOP-2192VOI2\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+
+        var sql = "SELECT * FROM Customers";
+        var customers = new List<CustomerModel>();
+        using (var connection = new SqlConnection(connString))
+        {
+            connection.Open();
+
+            customers = connection.Query<CustomerModel>(sql).ToList();
+        }
+
+        foreach (var customer in customers)
+        {
+            Console.WriteLine($"{customer.ContactName}");
+        }
+    }
+}
